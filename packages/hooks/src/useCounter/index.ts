2f949abcc3f9494c7b42e930e6589e7272b30fcf
RealHeng <596484539@qq.com>
2022-01-05 10:53:36 +0800

    comment

--- packages/hooks/src/useCounter/index.ts
+++ packages/hooks/src/useCounter/index.ts
@@ -1,67 +1,67 @@
-import { useState } from 'react';
-import useMemoizedFn from '../useMemoizedFn';
+import { useState } from 'react'
+import useMemoizedFn from '../useMemoizedFn'
 
 export interface Options {
-  min?: number;
-  max?: number;
+  min?: number
+  max?: number
 }
 
 export interface Actions {
-  inc: (delta?: number) => void;
-  dec: (delta?: number) => void;
-  set: (value: number | ((c: number) => number)) => void;
-  reset: () => void;
+  inc: (delta?: number) => void
+  dec: (delta?: number) => void
+  set: (value: number | ((c: number) => number)) => void
+  reset: () => void
 }
 
-export type ValueParam = number | ((c: number) => number);
+export type ValueParam = number | ((c: number) => number)
 
 function getTargetValue(val: number, options: Options = {}) {
-  const { min, max } = options;
-  let target = val;
+  const { min, max } = options
+  let target = val
   if (typeof max === 'number') {
-    target = Math.min(max, target);
+    target = Math.min(max, target)
   }
   if (typeof min === 'number') {
-    target = Math.max(min, target);
+    target = Math.max(min, target)
   }
-  return target;
+  return target
 }
 
 function useCounter(initialValue: number = 0, options: Options = {}) {
-  const { min, max } = options;
+  const { min, max } = options
 
   const [current, setCurrent] = useState(() => {
     return getTargetValue(initialValue, {
       min,
       max,
-    });
-  });
+    })
+  })
 
   const setValue = (value: ValueParam) => {
     setCurrent((c) => {
-      const target = typeof value === 'number' ? value : value(c);
+      const target = typeof value === 'number' ? value : value(c)
       return getTargetValue(target, {
         max,
         min,
-      });
-    });
-  };
+      })
+    })
+  }
 
   const inc = (delta: number = 1) => {
-    setValue((c) => c + delta);
-  };
+    setValue((c) => c + delta)
+  }
 
   const dec = (delta: number = 1) => {
-    setValue((c) => c - delta);
-  };
+    setValue((c) => c - delta)
+  }
 
   const set = (value: ValueParam) => {
-    setValue(value);
-  };
+    setValue(value)
+  }
 
   const reset = () => {
-    setValue(initialValue);
-  };
+    setValue(initialValue)
+  }
 
   return [
     current,
@@ -71,7 +71,7 @@ function useCounter(initialValue: number = 0, options: Options = {}) {
       set: useMemoizedFn(set),
       reset: useMemoizedFn(reset),
     },
-  ] as const;
+  ] as const
 }
 
-export default useCounter;
+export default useCounter
--- packages/hooks/src/useCreation/demo/demo1.tsx
+++ packages/hooks/src/useCreation/demo/demo1.tsx
@@ -6,31 +6,31 @@
  * desc.zh-CN: 点击 "Rerender" 按钮，触发组件的更新，但 Foo 的实例会保持不变
  */
 
-import React, { useState } from 'react';
-import { useCreation } from 'ahooks';
+import React, { useState } from 'react'
+import { useCreation } from 'ahooks'
 
 class Foo {
   constructor() {
-    this.data = Math.random();
+    this.data = Math.random()
   }
 
-  data: number;
+  data: number
 }
 
 export default function () {
-  const foo = useCreation(() => new Foo(), []);
-  const [, setFlag] = useState({});
+  const foo = useCreation(() => new Foo(), [])
+  const [, setFlag] = useState({})
   return (
     <>
       <p>{foo.data}</p>
       <button
         type="button"
         onClick={() => {
-          setFlag({});
+          setFlag({})
         }}
       >
         Rerender
       </button>
     </>
-  );
+  )
 }
